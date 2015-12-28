3b5a5a91dd4a956577b72a7fd4e7b13d790520ee
Harry Felton <hbomb79@users.noreply.github.com>
2015-12-28 17:12:24 +1300

    Refined comments

--- example/example.lua
+++ example/example.lua
@@ -12,13 +12,13 @@ local stage = app + Stage({
 
 -- this or
 
---[[local myPanel = stage + Panel( 2, 2, 16, 5 )
+--[[local myPanel = stage + Panel( 2, 2, 16, 5 ) -- fixed order of arguments (in this case X, Y, width, height). Also no other arguments can be in here, only these.
 myPanel.backgroundColour = colours.grey]]
 
 
 -- this code could be used
 local myPanel = stage + Panel({
-    Y = 2, -- any order of arguments
+    Y = 2, -- any order of arguments. Arguments not needed by the contructor can also be set (eg. backgroundColour)
     X = 2,
     height = 5,
     width = 16,
@@ -26,6 +26,6 @@ local myPanel = stage + Panel({
 })
 
 local myButton = myPanel + Button( "Hello World", 3, 1, 9, 4 )
-local myButton2 = myPanel + Button( "Hello Again", 3, 10, 9, 4 )
+local myButton2 = myPanel + Button( "Hello Again", 3, 10, 9, 4 ) -- tables can also be used to specify arguments with buttons, maybe you should try it out. Hop on Gitter if you have troubles
 
 app:run()
