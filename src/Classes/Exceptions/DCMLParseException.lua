f1044a0bf353e386ea50400077fd34573eebf831
hbomb79 <harryfelton12@gmail.com>
2016-02-01 10:44:16 +1300

    DCMLParseException added

---
+++ src/Classes/Exceptions/DCMLParseException.lua
@@ -0,0 +1,4 @@
+class "DCMLParseException" extends "ExceptionBase" {
+    title = "DCML Parser Exception";
+    subTitle = "This exception occurred while parsing, or handling parsed DCML content."
+}
