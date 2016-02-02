e249f171683b0cde5bafe02dfc593af6b420eebd
hbomb79 <harryfelton12@gmail.com>
2016-02-02 15:01:15 +1300

    Fixed TextContainer setText

--- src/Classes/Node/TextContainer.lua
+++ src/Classes/Node/TextContainer.lua
@@ -16,12 +16,13 @@ end
 function TextContainer:setText( text )
     self.text = text
 
-    if self.__init_complete then
+    if self.container then
         self:parseIdentifiers()
         self.container:cacheSegmentInformation()
+        self.recacheAllNextDraw = true
 
         -- Because the user may have been scrolling when the text changed, make sure that the Y offset isn't too big for this text.
-        self.verticalScroll = math.max( math.min( self.verticalScroll, self.container.height - 1 ), 0 )
+        self.verticalScroll = math.max( math.min( self.yOffset, self.container.height - 1 ), 0 )
 
         self.changed = true
     end
