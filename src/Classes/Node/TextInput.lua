8ddffbb4492a45b098ba084f04dc45ef93f650a8
hbomb79 <harryfelton12@gmail.com>
2016-02-02 20:39:34 +1300

    Fixed TextInput cursor
    When moving around newlines with the cursor some weird things happened.
    
    These weird things don't happen anymore, woo

--- src/Classes/Node/TextInput.lua
+++ src/Classes/Node/TextInput.lua
@@ -80,21 +80,19 @@ function TextInput:getCursorInformation()
     local lines = self.container.lines
 
     local remainingOffset = self.cursorPosition
-    local yAmount = 0
     local newlineOffset = 0;
     -- Use the cursor position to calculate the active line and the position on that line.
-    local line, currentLine
+    local line
     for y = 1, #lines do
         line = lines[ y ]
 
-        if remainingOffset > #line and y ~= #lines then
+        if remainingOffset - 1 > #line then
             cursorY = cursorY + 1
-            yAmount = yAmount + 1
-            remainingOffset = math.max( remainingOffset - #line, 0 )
+            remainingOffset = math.max( remainingOffset - #line - 1, 0 )
 
             if line.isNewline then newlineOffset = newlineOffset + 1 end
         else
-            cursorX = line.X + remainingOffset - 1 - newlineOffset
+            cursorX = line.X + remainingOffset - newlineOffset + cursorY - 2
             break
         end
     end
