d5f7b74d680be28a75f010230280661b0a6b7d51
hbomb79 <harryfelton12@gmail.com>
2016-02-13 16:23:55 +1300

    Anchor changes (actually works now)

--- src/Classes/Core/Anchor.lua
+++ src/Classes/Core/Anchor.lua
@@ -1,31 +1,67 @@
 class "Anchor" {
     target = nil;
-    properties = {};
     parent = nil;
+
+    config = {
+        top = false,
+        bottom = false,
+        left = false,
+        right = false,
+
+        maximumWidth = false,
+        maximumHeight = false,
+
+        minimumWidth = false,
+        minimumHeight = false
+    };
 }
 
 
 function Anchor:initialise( target )
-    self.target = classLib.isInstance( target ) or not target.anchorable or ConstructorException("Target is not a anchorable class")
-    self.parent = self.target.stage or self.target.parent
+    self.target = classLib.isInstance( target ) and target.anchorable and target or ConstructorException("Target is not a anchorable class")
+    self.parent = self.target.parent or self.target.stage
 end
 
 function Anchor:addPropertyTarget( property, value )
     -- This property will stay at the value from the side of the parent.
     if self.target and self.target.anchorable then
-        if self.target:isAnchorable( property, value ) then
-            self.properties[ property ] = value
-        else
-            return Exception("Invalid property target '"..tostring( property ).."' to value '"..tostring( value ).."'")
-        end
+        if not self.target:isPropertyAnchorable( property, value ) then return ParameterException("Cannot anchor property '"..property.."' with value '"..tostring( value ).." (type: "..type( value )..")'") end
+
+        self.config[ property ] = value
     else
         return Exception("Anchor target is missing or not achorable: '"..tostring( self.target ).."'")
     end
 end
 
 function Anchor:updateAnchor()
-    -- Parent size has changed, lets adjust this node
-    for property, target in self.properties do
-        self.target[ property ] = self.target[ property - ( target - 1 ) ]
+    local config, target, parent = self.config, self.target, self.parent
+
+    -- Sub anchors. No parent calculations required as the co-ordinates are relative to the top corner.
+    if config.top then target.Y = config.top + 1 end
+    if config.left then target.X = config.left + 1 end
+
+    -- Main anchors. Parent calculations required.
+    if config.bottom then
+        if config.top then
+            local desiredHeight = parent.height + 1 - target.Y - config.bottom
+            local maxH, minH = config.maximumHeight or desiredHeight, config.minimumHeight or desiredHeight
+
+
+            desiredHeight = desiredHeight > maxH and maxH or ( desiredHeight < minH and minH ) or desiredHeight
+            target.height = desiredHeight < 1 and 1 or desiredHeight
+        else
+            target.Y = parent.height + 1 - config.bottom - target.height
+        end
+    end
+    if config.right then
+        if config.left then
+            local desiredWidth = parent.width + 1 - target.X - config.right
+            local maxW, minW = config.maximumWidth or desiredWidth, config.minimumWidth or desiredWidth
+
+            desiredWidth = desiredWidth > maxW and maxW or ( desiredWidth < minW and minW ) or desiredWidth
+            target.width = desiredWidth < 1 and 1 or desiredWidth
+        else
+            target.X = parent.width + 1 - config.right - target.width
+        end
     end
 end
